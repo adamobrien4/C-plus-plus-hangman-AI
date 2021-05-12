315d935b6cfaed999178edfbf06df3b420746683
Adam O'Brien <17226864@studentmail.ul.ie>
2021-05-12 10:02:53 +0100

    Restructured application
    Added basic Agent functionality

--- main.cpp
+++ main.cpp
@@ -7,13 +7,134 @@
 
 using namespace std;
 
+class Agent
+{
+public:
+    Agent() { }
+
+    void setWordLength(int length) {
+        wordLength = length;
+        for(int i = 0; i < length; i++) {
+            progress += '?';
+        }
+    }
+
+    int getCorrectChars() {
+        return correctChars;
+    }
+
+    void decrementLives() {
+        lives--;
+    }
+    void setLives(int l) {
+        lives = l;
+    }
+    int getLives() {
+        return lives;
+    }
+
+    int getGuessCount() {
+        return guessCounter;
+    }
+
+    virtual char makeGuess() {
+        cout << "Base agent making guess" << endl;
+    }
+    void updateProgress(int index, char letter) {
+        progress[index] = letter;
+        correctChars++;
+    }
+    string getProgress() {
+        return progress;
+    }
+protected:
+    string progress;
+    int wordLength {0};
+    int correctChars {0};
+    int lives {0};
+    int guessCounter {0};
+};
+
+// ------ Global Variables
+
+vector<string> words;
+string word;
+Agent *agent;
+bool debug = true;
+
+// ------ Class Definitions
+
+class Human : public Agent
+{
+public:
+    char makeGuess() {
+        char guess;
+        cout << "You have " << lives << " remaining guesses." << endl;
+        cout << progress << endl;
+        cout << "Make a guess: ";
+        cin >> guess;
+
+        guessCounter++;
+        return guess;
+    }
+};
+
+class AgentOne : public Agent
+{
+public:
+    char makeGuess() {
+        guessCounter++;
+        return (rand() % 26) + 'a';
+    }
+};
+
+vector<pair<char, float>> read_csv_pair(string filename) {
+    vector<pair<char, float>> result;
+
+    ifstream myFile(filename);
+    if(!myFile.is_open()) throw runtime_error("Could not open file");
+
+    // Helper vars
+    string line;
+    string val;
+
+    // Read data, line by line
+    while(getline(myFile, line))
+    {
+        char c = line.substr(0, 1)[0];
+        float f = stof(line.substr(2));
+        result.push_back(make_pair(c, f));
+    }
+
+    // Close file
+    myFile.close();
+
+    return result;
+}
+
+class AgentTwo : public Agent
+{
+public:
+    AgentTwo() {
+        letterDist = read_csv_pair("letter-dist.csv");
+    }
+    char makeGuess() {
+        char guess = letterDist[guessCounter].first;
+        guessCounter++;
+        return guess;
+    }
+private:
+    vector<pair<char, float>> letterDist;
+};
+
+
+// ------ Program Functions
+
+// TODO: Make generic read_csv function to handle different types of values
 vector<string> read_csv(string filename){
     vector<string> result;
 
-    // Create an input filestream
     ifstream myFile(filename);
-
-    // Make sure the file is open
     if(!myFile.is_open()) throw runtime_error("Could not open file");
 
     // Helper vars
@@ -50,46 +171,94 @@ vector<string> read_csv(string filename){
     return result;
 }
 
+bool guess_letter(char letter) {
+
+    if(debug) {
+        cout << "Guess was " << letter << endl;
+    }
+
+    bool correct_guess = false;
+    for(int i = 0; i < word.length(); i++) {
+        if(word[i] == letter) {
+            agent->updateProgress(i, letter);
+            correct_guess = true;
+        }
+    }
+
+    if(!correct_guess) {
+        agent->decrementLives();
+    }
+
+    if(agent->getCorrectChars() == word.size()) {
+        return true;
+    }
+    return false;
+}
+
+// ------ Main Function
+
 int main()
 {
     srand(time(NULL));
 
-    vector<string> words = read_csv("english_words.csv");
-    string word = words[rand() % words.size()];
+    words = read_csv("english_words.csv");
+    word = words[rand() % words.size()];
 
-    // Holds how much of the word the user has guessed correctly
-    string progress (word.length(), '?');
-    int correct_chars = 0;
-    char guess;
-    int lives = 6;
+    // Load CSV file containing letter distribution from https://en.wikipedia.org/wiki/Letter_frequency - Relative frequency in the English language on Dictionaries.
 
-    while(lives > 0) {
-        cout << "----------------------------------------" << endl;
-        cout << "Remaining Guesses: " << lives << endl;
-        cout << progress << endl;
-        cout << "Guess a letter: ";
-        cin >> guess;
+    int game_mode;
 
-        bool correct_guess = false;
-        for(int i = 0; i < word.length(); i++) {
-            if(word[i] == guess) {
-                progress[i] = guess;
-                correct_chars++;
-                correct_guess = true;
+    cout << "Game Mode:" << endl;
+    cout << "0: Player vs. Computer" << endl;
+    cout << "1: Agent 1" << endl;
+    cout << "2: Agent 2" << endl;
+    cout << "3: Agent 3" << endl;
+    cin >> game_mode;
+
+    switch(game_mode) {
+        case 0:
+            {
+                // Holds how much of the word the user has guessed correctly
+                agent = new Human();
+                cout << "Mode 0" << endl;
+                break;
             }
-        }
+        case 1:
+            {
+                agent = new AgentOne();
+                cout << "Mode 1" << endl;
+                break;
+            }
+        case 2:
+            {
+                agent = new AgentTwo();
+                cout << "Mode 2" << endl;
+                break;
+            }
+        case 3:
+            cout << "Mode 3" << endl;
+            break;
+        default:
+            cout << "Invalid game mode selected.";
+            return 0;
+    }
 
-        if(!correct_guess) {
-            lives--;
-        }
+    agent->setWordLength(word.length());
+    agent->setLives(8);
 
-        if(correct_chars == word.size()) {
-            cout << "Congratulations! The word was " << word << endl;
+    cout << "Starting Game" << endl;
+
+    while(agent->getLives() > 0) {
+        if(guess_letter(agent->makeGuess())) {
+            cout << "The word was " << word << endl;
+            cout << "Congratulations! Game completed in " << agent->getGuessCount() << " guesses."  << endl;
+            cout << "Agent's progress " << agent->getProgress() << endl;
             return 0;
         }
     }
 
     cout << "The word was " << word << ". Game Over!" << endl;
+    cout << "Agent's progress " << agent->getProgress() << endl;
 
     return 0;
 }
